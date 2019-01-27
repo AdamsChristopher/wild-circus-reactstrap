779c507e78be727a8d416feecc11deb234f9a370
AdamsChristopher <neanderthalnoise@gmail>
2019-01-27 16:40:39 +0100

    Changed contact form appearence

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, CardBody, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 import '../styles/ContactForm.css';
 
@@ -22,50 +22,52 @@ class ContactForm extends Component {
         id="Contact-Form"
         className="clearfix my-5"
       >
-        <Form onSubmit={this.handleSubmit}>
-          <FormGroup>
-            <Label for="FirstName">First Name:</Label>
-            <Input
-              id="FirstName"
-              type="text"
-              name="text"
-              placeholder="Enter your first name"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="LastName">Last Name:</Label>
-            <Input
-              id="LastName"
-              type="text"
-              name="text"
-              placeholder="Enter your last name"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Email">Email:</Label>
-            <Input
-              id="Email"
-              type="email"
-              name="email"
-              placeholder="Enter your email"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Message">Message:</Label>
-            <Input
-              id="Message"
-              type="textarea"
-              name="text"
-              placeholder="Enter your message here"
-            />
-          </FormGroup>
-          <Button
-            id="Button"
-            className="float-right"
-          >
-            Send
+        <CardBody className="Card-Body">
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="FirstName">First Name:</Label>
+              <Input
+                id="FirstName"
+                type="text"
+                name="text"
+                placeholder="Enter your first name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="LastName">Last Name:</Label>
+              <Input
+                id="LastName"
+                type="text"
+                name="text"
+                placeholder="Enter your last name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="Email">Email:</Label>
+              <Input
+                id="Email"
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="Message">Message:</Label>
+              <Input
+                id="Message"
+                type="textarea"
+                name="text"
+                placeholder="Enter your message here"
+              />
+            </FormGroup>
+            <Button
+              id="Button"
+              className="float-right"
+            >
+              Send
           </Button>
-        </Form>
+          </Form>
+        </CardBody>
       </Container>
     );
   }
