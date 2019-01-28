84d4bad092f411534d3302bc6455cda47ad43479
AdamsChristopher <neanderthalnoise@gmail.com>
2019-01-28 22:11:31 +0100

    Added controlled inputs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -7,9 +7,20 @@ class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      first: '',
+      last: '',
+      email: '',
+      message: ''
     };
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit(event) {
@@ -17,6 +28,12 @@ class ContactForm extends Component {
   }
 
   render() {
+    const {
+      first,
+      last,
+      email,
+      message
+    } = this.state;
     return (
       <Container
         id="Contact-Form"
@@ -27,36 +44,44 @@ class ContactForm extends Component {
             <FormGroup>
               <Label for="FirstName">First Name:</Label>
               <Input
+                onChange={this.handleChange}
                 id="FirstName"
                 type="text"
-                name="text"
+                name="first"
+                value={first}
                 placeholder="Enter your first name"
               />
             </FormGroup>
             <FormGroup>
               <Label for="LastName">Last Name:</Label>
               <Input
+                onChange={this.handleChange}
                 id="LastName"
                 type="text"
-                name="text"
+                name="last"
+                value={last}
                 placeholder="Enter your last name"
               />
             </FormGroup>
             <FormGroup>
               <Label for="Email">Email:</Label>
               <Input
+                onChange={this.handleChange}
                 id="Email"
                 type="email"
                 name="email"
+                value={email}
                 placeholder="Enter your email"
               />
             </FormGroup>
             <FormGroup>
               <Label for="Message">Message:</Label>
               <Input
+                onChange={this.handleChange}
                 id="Message"
                 type="textarea"
-                name="text"
+                name="message"
+                value={message}
                 placeholder="Enter your message here"
               />
             </FormGroup>
